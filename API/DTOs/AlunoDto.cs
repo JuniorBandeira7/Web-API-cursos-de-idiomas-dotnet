b375192dfe5f50992759e7d85b1ca983ec098616
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-10-21 16:11:02 -0300

    feat: Criação dos DTOs

---
+++ API/DTOs/AlunoDto.cs
@@ -0,0 +1,7 @@
+namespace API.DTOs;
+public class AlunoDto
+{
+    public string Cpf { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+}
---
+++ API/DTOs/TurmaDto.cs
@@ -0,0 +1,7 @@
+namespace API.DTOs;
+public class TurmaDto
+{
+    public string Codigo { get; set; }
+
+    public string nivel { get; set; }
+}
